5c4a7bc6e210a3139fd7c418700a9fc329601acf
Arabella Jordan <jordanfaearabella@gmail.com>
2025-02-04 21:39:52 +0800

    axios

--- server/src/app.js
+++ server/src/app.js
@@ -8,9 +8,9 @@ app.use(morgan('combined')); // logs the device that accesses the endpoint
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/status', (req, res) => {
+app.post('/register', (req, res) => {
     res.send({
-        message: 'Hello'
+        message: `user ${req.body.email} is now registered`
     });
 });
 app.listen(process.env.PORT || 8081);
